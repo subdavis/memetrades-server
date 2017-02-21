fa27ad33029cb41d7f7aef583eb3fca9312b0e56
Brandon Davis <bdavis@redspin.net>
2017-02-21 18:23:42 -0500

    Whatever yo

--- process_queue.py
+++ process_queue.py
@@ -2,13 +2,23 @@ import memeServer
 import time
 from memeServer import models
 
+# This code handles queue processing in a single thread....
+# Is it good?  No.
+# Is it better than before?  Honestly IDK.  It works tho.
+
 while True:
     
-    transactions = models.TransactionBacklog.objects().order_by('time')
+    try:
+        transactions = models.TransactionBacklog.objects().order_by('time')
+        
+        for t in transactions:
+            t.process()
+        for t in transactions:
+            t.delete()
+        
+        time.sleep(.1)
     
-    for t in transactions:
-        t.process()
-    for t in transactions:
-        t.delete()
-    
-    time.sleep(.1)
+    except Exception as e:
+        print("---- EXCEPTION ----")
+        print(e)
+        print("-------------------")
