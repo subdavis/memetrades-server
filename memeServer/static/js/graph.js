c8c1372283d1f8c53995e3e125bf258aca647920
Brandon Davis <bdavis@redspin.net>
2017-02-27 21:11:57 -0500

    Better graphing

--- memeServer/static/js/graph.js
+++ memeServer/static/js/graph.js
@@ -1,47 +1,72 @@
 function graph(meme, base_url) {
-  $.getJSON(base_url + "/api/history", {meme:meme}, function(input) {
-
-    if (input.length == 0){
-      console.log("Nothing to graph...");
-      return;
+  $.getJSON("/api/history?meme=" + meme, function (data) {
+    var formattedData = new Array();
+    
+    // format data to [time, price] format
+    for (var trade in data) {
+      formattedData.push([data[trade].time * 1000, data[trade].price]);
     }
     
-    var dataPoints= input.map(function(pt) { 
-      return {
-        x:new Date(Math.round(pt["time"]*1000)), 
-        y:pt["price"]
-      };
-    });
-    
-    
-    // Create a data point at now.
-    dataPoints.unshift({
-      x:new Date(Date.now()), 
-      y:dataPoints[0]["y"]
-    });
+    Highcharts.chart('chartContainer', {
+        chart: {
+            panning: true,
+            // hold shift to pan
+            panKey: 'shift',
+            zoomType: 'x'
+        },
+        title: {
+            text: ""
+        },
+        subtitle: {
+            text: document.ontouchstart === undefined ?
+                    'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'Stock value'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                // higher the cropThreshold, the more points you can graph
+                // while retaining zoom functionality
+                cropThreshold: 1000,
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
 
-    var chart = new CanvasJS.Chart(
-      "chartContainer",
-      {
-        axisX:{
-          title: "time",
-          gridThickness: 2,
-          // interval:interval, 
-          intervalType: "hour",        
-          valueFormatString: "MMM DD hh TT", 
-          labelAngle: -20
-        },
-        axisY:{
-          title: "price"
-        },
-        data: [
-          {        
-            type: "line",
-            lineThickness: 3,
-            dataPoints: dataPoints
-          }
-        ]
-      });
-    chart.render();
+        series: [{
+            type: 'area',
+            name: 'Stock value',
+            data: formattedData
+        }]
+    });
   });
 }
