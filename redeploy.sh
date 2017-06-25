2da6a61784927adc81b5d562267a396e05fb1913
Brandon Davis <brandon@subdavis.com>
2017-06-24 21:32:57 -0400

    redeploy

---
+++ redeploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker rm memect && docker rmi memes
+docker docker build -f Dockerfile.web -t memes . 
+docker run -e "VIRTUAL_HOST=memetrades.com" -e "LETSENCRYPT_HOST=memetrades.com," -e "LETSENCRYPT_EMAIL=developers@memetrades.com" -dt --link mongo:mongo --name memect memes
