2f59d673445a53b5fdf35083b522612d52c9e705
Brandon Davis <brandon@subdavis.com>
2017-06-24 21:52:30 -0400

    Needs more memes

--- readme.md
+++ readme.md
@@ -21,7 +21,7 @@ MemeTrades.com
 
 `docker build -f Dockerfile.web -t memes .`
 
-`docker run -e "VIRTUAL_HOST=memetrades.com" -e "LETSENCRYPT_HOST=memetrades.com," -e "LETSENCRYPT_EMAIL=developers@memetrades.com" -dt --link mongo:mongo --name memect memes`
+`docker run -e "VIRTUAL_HOST=memetrades.com" -e "LETSENCRYPT_HOST=memetrades.com," -e "LETSENCRYPT_EMAIL=developers@memetrades.com" -dt -v /certs:/certs:rw --link mongo:mongo --name memect memes`
 
 ### Feature requests or bug reports:
 
--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker rm memect && docker rmi memes
 docker build -f Dockerfile.web -t memes . 
-docker run -e "VIRTUAL_HOST=memetrades.com" -e "LETSENCRYPT_HOST=memetrades.com" -e "LETSENCRYPT_EMAIL=developers@memetrades.com" -dt --link mongo:mongo --name memect memes
+docker run \ 
+	-e "VIRTUAL_HOST=memetrades.com" \  
+	-e "LETSENCRYPT_HOST=memetrades.com" \ 
+	-e "LETSENCRYPT_EMAIL=developers@memetrades.com" \ 
+	-dt -v /certs:/certs:rw --link mongo:mongo --name memect memes
